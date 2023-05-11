c95d331137057b4c7837e715b225c07c1e14e47c
Nicholas Yousefi <nsyousef@ucsd.edu>
2023-05-11 00:00:44 -0700

    Fix typo in name of local storage item

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,7 +24,7 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
-  return eval(localStorage.getItem("recipies"));
+  return eval(localStorage.getItem("recipes"));
 }
 
 /**
